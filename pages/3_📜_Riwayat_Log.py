d7bb4b4af6689a8c74420649625f97b4fc47427a
hukiMoon <irfannrmdnn@gmail.com>
2026-07-07 15:03:22 +0700

    Update 3_📜_Riwayat_Log.py

--- pages/3_📜_Riwayat_Log.py
+++ pages/3_📜_Riwayat_Log.py
@@ -12,12 +12,12 @@ def jalankan_query(sql, param=(), commit=False):
     if not commit: data = cursor.fetchall()
     return data
 
-    st.title("📜 Log Aktivitas Gudang")
-    st.write("---")
+st.title("📜 Log Aktivitas Gudang")
+st.write("---")
     
-    data_riwayat = jalankan_query("SELECT kode_barang, nama_barang, jenis_transaksi, jumlah, satuan, tanggal, keterangan FROM riwayat ORDER BY id DESC")
-    if not data_riwayat:
-        st.info("Belum ada riwayat transaksi.")
-    else:
-        df = pd.DataFrame(data_riwayat, columns=["Kode Barang", "Nama Barang", "Jenis Transaksi", "Jumlah", "Satuan", "Tanggal Transaksi", "Keterangan"])
-        st.dataframe(df, hide_index=True, use_container_width=True)
+data_riwayat = jalankan_query("SELECT kode_barang, nama_barang, jenis_transaksi, jumlah, satuan, tanggal, keterangan FROM riwayat ORDER BY id DESC")
+if not data_riwayat:
+    st.info("Belum ada riwayat transaksi.")
+else:
+    df = pd.DataFrame(data_riwayat, columns=["Kode Barang", "Nama Barang", "Jenis Transaksi", "Jumlah", "Satuan", "Tanggal Transaksi", "Keterangan"])
+    st.dataframe(df, hide_index=True, use_container_width=True)
