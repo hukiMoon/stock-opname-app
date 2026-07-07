0a7e8c7f77dc41c79e31631d3586702747e13e9e
hukiMoon <irfannrmdnn@gmail.com>
2026-07-07 12:24:28 +0700

    Create 3_📜_Riwayat_Log.py

---
+++ pages/3_📜_Riwayat_Log.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import psycopg2
+import pandas as pd
+
+DB_URL = "postgresql://postgres.krckbruwpxgiziujgqiy:1P%40ny001%2E%2E%2E@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres"
+
+def jalankan_query(sql, param=(), commit=False):
+    conn = psycopg2.connect(DB_URL)
+    cursor = conn.cursor()
+    cursor.execute(sql, param)
+    data = None
+    if not commit: data = cursor.fetchall()
+    return data
+
+if st.query_params.get("session") != "loggedin":
+    st.warning("Silakan login terlebih dahulu di halaman utama!")
+else:
+    st.title("📜 Log Aktivitas Gudang")
+    st.write("---")
+    
+    data_riwayat = jalankan_query("SELECT kode_barang, nama_barang, jenis_transaksi, jumlah, satuan, tanggal, keterangan FROM riwayat ORDER BY id DESC")
+    if not data_riwayat:
+        st.info("Belum ada riwayat transaksi.")
+    else:
+        df = pd.DataFrame(data_riwayat, columns=["Kode Barang", "Nama Barang", "Jenis Transaksi", "Jumlah", "Satuan", "Tanggal Transaksi", "Keterangan"])
+        st.dataframe(df, hide_index=True, use_container_width=True)
