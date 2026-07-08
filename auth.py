e155e12434ba0661cfd46f326eabd357471dd783
hukiMoon <irfannrmdnn@gmail.com>
2026-07-08 08:29:31 +0700

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,18 @@
+# auth.py
+import streamlit as st
+
+def check_password():
+    """Fungsi untuk mengecek apakah user sudah login."""
+    if "login_sukses" not in st.session_state:
+        st.session_state.login_sukses = False
+
+    if not st.session_state.login_sukses:
+        st.title("🔐 Login Admin")
+        password = st.text_input("Masukkan Password", type="password")
+        if st.button("Login"):
+            if password == "admin123":  # Ganti dengan password yang diinginkan
+                st.session_state.login_sukses = True
+                st.rerun()
+            else:
+                st.error("Password salah!")
+        st.stop() # Menghentikan sisa kode agar tidak tampil sebelum login
