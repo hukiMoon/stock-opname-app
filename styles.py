3ea9f095caf9a60b717bca51251b673cc057b58a
hukiMoon <irfannrmdnn@gmail.com>
2026-07-08 13:23:24 +0700

    Update styles.py

--- styles.py
+++ styles.py
@@ -2,6 +2,10 @@
 import streamlit as st
 
 def card_container(title):
+    """
+    Membuat container dengan border dan judul agar tampilan 
+    setiap menu terlihat seragam dan profesional.
+    """
     container = st.container(border=True)
     container.markdown(f"### {title}")
     return container
