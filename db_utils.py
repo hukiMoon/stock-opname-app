36440f240e1f3be1c2017707c959ff841e20f7d8
hukiMoon <irfannrmdnn@gmail.com>
2026-07-08 14:41:26 +0700

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extras
 import streamlit as st
+import bcrypt
 
 # Gunakan secrets untuk keamanan (opsional tapi sangat disarankan)
 # Jika tidak ingin pakai secrets, masukkan URL di sini
@@ -30,3 +31,11 @@ def get_stok_rendah(batas=5):
     # Mengambil barang yang stoknya kurang dari atau sama dengan batas
     query = "SELECT nama_barang, stok_sistem FROM barang WHERE stok_sistem <= %s"
     return jalankan_query(query, (batas,))
+
+def register_user(username, password, role):
+    # Hash password sebelum disimpan
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    
+    query = "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)"
+    # jalankan_query(query, (username, hashed.decode('utf-8'), role))
