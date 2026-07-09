ced4a4f5f066087572c93a647a538124cdd5fe07
hukiMoon <irfannrmdnn@gmail.com>
2026-07-09 14:24:25 +0700

    Update Beranda.py

--- Beranda.py
+++ Beranda.py
@@ -27,7 +27,7 @@ else:
     password = st.text_input("Password", type="password")
     
     if st.button("Login"):
-        if username == "admin" and password == "admin":
+        if username == "admin" and password == "admin123":
             st.session_state["logged_in"] = True
             st.session_state["current_page"] = "beranda"
             st.rerun()
