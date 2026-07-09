529c9a089c03e31f418cbd6901fa58fbe5d5b158
hukiMoon <irfannrmdnn@gmail.com>
2026-07-09 12:30:56 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -35,11 +35,11 @@ def tampilkan_sidebar():
         st.sidebar.subheader("Menu Admin")
         
         # Tambahkan SEMUA halaman Anda di sini
-        st.sidebar.page_link("pages/1_📥_Barang_Masuk.py")
-        st.sidebar.page_link("pages/2_📥_Barang_Keluar.py")
-        st.sidebar.page_link("pages/3_📜_Riwayat_Log.py")
-        st.sidebar.page_link("pages/4_⚙️_Master_Barang.py")
-        st.sidebar.page_link("pages/5_📈_Statistik.py")
+        st.sidebar.page_link("pages/Barang Masuk.py")
+        st.sidebar.page_link("pages/Barang Keluar.py")
+        st.sidebar.page_link("pages/Riwayat Log.py")
+        st.sidebar.page_link("pages/Master Barang.py")
+        st.sidebar.page_link("pages/Statistik.py")
     
     st.sidebar.divider()
     
