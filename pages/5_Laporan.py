fc5742f005354b5e3e76f3848150ca20a6d3c583
hukiMoon <irfannrmdnn@gmail.com>
2026-07-20 15:33:32 +0700

    Update 5_Laporan.py

--- pages/5_Laporan.py
+++ pages/5_Laporan.py
@@ -11,7 +11,7 @@ from db_utils import (
     jalankan_perintah_db,
     ambil_data_log
 )
-from utils import check_login, check_role, tampilkan_sidebar
+from utils import check_login, tampilkan_sidebar
 
 # Konfigurasi Halaman
 check_login()
