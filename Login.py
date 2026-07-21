b8729f9aaf5f43b8a067fd0a3dc3f230bbd36bee
hukiMoon <irfannrmdnn@gmail.com>
2026-07-21 08:31:15 +0700

    Update Login.py

--- Login.py
+++ Login.py
@@ -2,8 +2,12 @@ import streamlit as st
 import setup_db
 from utils import init_login_state, show_login
 
-# 1. Konfigurasi halaman dasar
-st.set_page_config(page_title="Login - Stock Opname", layout="centered")
+# 1. Konfigurasi halaman dasar (Tambahkan initial_sidebar_state="collapsed")
+st.set_page_config(
+    page_title="Login - Stock Opname", 
+    layout="centered",
+    initial_sidebar_state="collapsed" 
+)
 
 # 2. Inisialisasi database (menjalankan setup user pertama kali jika belum ada)
 setup_db.inisialisasi_user_awal()
@@ -14,9 +18,7 @@ init_login_state()
 # 4. Logika Pengalihan Halaman
 if not st.session_state["logged_in"]:
     # Jika belum login, tampilkan form login
-    # Catatan: Fungsi show_login() di utils.py harus sudah berisi CSS 
-    # penyembunyi sidebar seperti yang kita bahas sebelumnya.
     show_login()
 else:
-    # Jika sudah login, langsung "lempar" atau alihkan ke halaman Beranda
+    # Jika sudah login, langsung alihkan ke halaman Beranda
     st.switch_page("pages/0_Beranda.py")
